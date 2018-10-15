668107f12296c90e20c5cd6df520ec9a0a18cae9
SQLDoubleG <sqldoubleg@gmail.com>
2018-10-15 20:38:25 +0100

    Changes to BackupSizeInfo, SP and script
    10/10/2018 RAG
    	- Added backup type X that will all types expect LOG backups
    14/10/2018 RAG
    	- Modified the restore command to consider RESTORE LOG
    	- Modified the restore command to be by default WITH NORECOVERY
    	- Added @orderBy to sort ascending or descending to allow copy paste the restore commands in the right order
    	- Changed order of the returned columns to have finished date at first glance

--- Scripts/BackupSizeInfo.sql
+++ Scripts/BackupSizeInfo.sql
@@ -25,14 +25,19 @@ GO
 --				@onlyFullBkp, set to 1 to display only full backups, if set to 0, will display any backup
 --				@numBkp, specify the number of backups to display per database (from newest to oldest)
 --
--- Log History:	25/09/2013 RAG 
---					Changed the query to display backups distributed in more than 1 file as 1 single backup
---					and physical_device_name will concatenate all files for that backup
---				19/12/2013 RAG Changed to use LIKE in the databases query to use wildcards in the parameter
---				04/08/2014 RAG Added a basic restore statement
---				26/04/2016 RAG Changed parameter @onlyFullBkp for @backupType
---				30/09/2018 RAG Created new Dependencies section
+-- Log History:	25/09/2013 RAG	- Changed the query to display backups distributed in more than 1 file as 1 single backup
+--									and physical_device_name will concatenate all files for that backup
+--				19/12/2013 RAG	- Changed to use LIKE in the databases query to use wildcards in the parameter
+--				04/08/2014 RAG	- Added a basic restore statement
+--				26/04/2016 RAG	- Changed parameter @onlyFullBkp for @backupType
+--				30/09/2018 RAG	- Created new Dependencies section
 --								- Added dependant function as tempdb object 
+--				10/10/2018 RAG	- Added backup type X that will all types expect LOG backups
+--				14/10/2018 RAG	- Modified the restore command to consider RESTORE LOG 
+-- 								- Modified the restore command to be by default WITH NORECOVERY
+--								- Added @orderBy to sort ascending or descending to allow copy paste 
+-- 									the restore commands in the right order
+--								- Changed order of the returned columns to have finished date at first glance
 -- =============================================
 -- =============================================
 -- Dependencies:This Section will create on tempdb any dependant function
@@ -65,10 +70,14 @@ GO
 DECLARE @dbname			SYSNAME = NULL
 		, @backupType	CHAR(1) = NULL
 		, @numBkp		INT = 1
+		, @orderBy		VARCHAR(10) = 'DESC'
 	
 SET NOCOUNT ON
 
-IF @backupType IS NOT NULL AND @backupType NOT IN ('D', 'I', 'L', 'F', 'G', 'P', 'Q') BEGIN
+SET @numBkp		= ISNULL(@numBkp, 1) 
+SET @orderBy	= ISNULL(@orderBy, 'DESC') 
+
+IF @backupType IS NOT NULL AND @backupType NOT IN ('D', 'I', 'L', 'F', 'G', 'P', 'Q', 'X') BEGIN
 	RAISERROR ('The parameter @backupType can only be one of these values
 D-> Database
 I-> Differential database
@@ -77,15 +86,22 @@ F-> File or filegroup
 G-> Differential file
 P-> Partial
 Q-> Differential partial
+X-> All types except LOG
 NULL -> All types will be included', 16, 0) WITH NOWAIT
 	GOTO OnError
 END
 
-SET @numBkp			= ISNULL(@numBkp, 1) 
+IF @orderBy NOT IN ('ASC', 'DESC') BEGIN
+	RAISERROR ('The parameter @orderBy can only be one of these values
+ASC-> To sort by date asceding
+DESC-> To sort by date asceding', 16, 0) WITH NOWAIT
+	GOTO OnError
+END
 
 ;WITH cte AS (
 	SELECT	b.server_name
 			, b.database_name
+			, b.type
 			,	CASE 
 					WHEN b.type = 'D' THEN 'Database'
 					WHEN b.type = 'I' THEN 'Differential database'
@@ -102,35 +118,37 @@ SET @numBkp			= ISNULL(@numBkp, 1)
 			, b.backup_finish_date
 			, b.media_set_id
 			--, mf.physical_device_name
-			, ROW_NUMBER() OVER (PARTITION BY database_name ORDER BY backup_set_id DESC) AS RowNum			
+			, ROW_NUMBER() OVER (PARTITION BY database_name 
+									ORDER BY CASE WHEN @orderBy = 'ASC' THEN backup_set_id ELSE NULL END ASC
+											, CASE WHEN @orderBy = 'DESC' THEN backup_set_id ELSE NULL END DESC) AS RowNum			
 		FROM msdb.dbo.backupset as b
 			--INNER JOIN msdb.dbo.backupmediafamily as mf
 			--	ON mf.media_set_id = b.media_set_id
 		WHERE database_name LIKE ISNULL(@dbname, database_name)
 			AND b.server_name = @@SERVERNAME
-			AND type = ISNULL(@backupType, type)
+			AND (type = ISNULL(@backupType, type) OR (@backupType = 'X' AND type <> 'L'))
 )
 
 SELECT 	server_name
 		, database_name
-		, STUFF( (SELECT ', ' + physical_device_name FROM msdb.dbo.backupmediafamily as mf WHERE mf.media_set_id = cte.media_set_id FOR XML PATH('')), 1,2,'' ) AS physical_device_name
 		, backup_type
-		, CASE WHEN has_backup_checksums = 1 THEN 'Yes' ELSE 'No' END AS has_backup_checksums
-		, CONVERT( INT, backup_size / 1024 /1024 ) AS size_MB
-		, CONVERT( INT, compressed_backup_size / 1024 /1024 ) AS compressed_size_MB
-		, CONVERT( DECIMAL(10,2), backup_size / 1024. /1024 /1024 ) AS size_GB
-		, CONVERT( DECIMAL(10,2), compressed_backup_size / 1024. / 1024 /1024 ) AS compressed_size_GB
-		, CONVERT( DECIMAL(10,2), 100 - ( ( compressed_backup_size * 100 ) / backup_size ) ) AS compression_ratio
-		, CONVERT( DECIMAL(10,2), (compressed_backup_size / 1024. / 1024) / (DATEDIFF(SECOND, backup_start_date, backup_finish_date ) + 1) ) AS MB_per_sec
-		, backup_finish_date			
+		, CONVERT(DATETIME2(0), backup_finish_date) AS backup_finish_date
+		, STUFF((SELECT ', ' + physical_device_name FROM msdb.dbo.backupmediafamily as mf WHERE mf.media_set_id = cte.media_set_id FOR XML PATH('')), 1,2,'' ) AS physical_device_name
+		, CONVERT(INT, backup_size / 1024 /1024 ) AS size_MB
+		, CONVERT(INT, compressed_backup_size / 1024 /1024 ) AS compressed_size_MB
+		, CONVERT(DECIMAL(10,2), backup_size / 1024. /1024 /1024 ) AS size_GB
+		, CONVERT(DECIMAL(10,2), compressed_backup_size / 1024. / 1024 /1024 ) AS compressed_size_GB
+		, CONVERT(DECIMAL(10,2), 100 - ( ( compressed_backup_size * 100 ) / backup_size ) ) AS compression_ratio
+		, CONVERT(DECIMAL(10,2), (compressed_backup_size / 1024. / 1024) / (DATEDIFF(SECOND, backup_start_date, backup_finish_date ) + 1) ) AS MB_per_sec
 		, tempdb.dbo.formatSecondsToHR( DATEDIFF(SECOND,backup_start_date, backup_finish_date) ) AS duration
-		, 'RESTORE DATABASE ' + QUOTENAME(database_name) + CHAR(10) + CHAR(9) + 'FROM ' + 
+		, CASE WHEN has_backup_checksums = 1 THEN 'Yes' ELSE 'No' END AS has_backup_checksums
+		, 'RESTORE ' + CASE WHEN type = 'L' THEN 'LOG ' ELSE 'DATABASE ' END + QUOTENAME(database_name) + CHAR(10) + CHAR(9) + 'FROM ' + 
 			STUFF( (SELECT ', DISK = ''' + physical_device_name + '''' + CHAR(10) + CHAR(9)
-						FROM msdb.dbo.backupmediafamily as mf WHERE mf.media_set_id = cte.media_set_id FOR XML PATH('')), 1,2,'' )  AS RESTORE_BACKUP
+						FROM msdb.dbo.backupmediafamily as mf WHERE mf.media_set_id = cte.media_set_id FOR XML PATH('')), 1,2,'' ) + 'WITH NORECOVERY' AS RESTORE_BACKUP
 	FROM cte
 	WHERE RowNum <= @numBkp
-	ORDER BY database_name ASC
-		, backup_finish_date DESC
+	ORDER BY database_name ASC, RowNum ASC
+
 OnError:
 GO
 -- =============================================
