0e4b2a13e84dec419b555157f790b089fa5b1277
raul-sqldoubleg <raul.gonzalez@linnworks.com>
2022-02-25 14:54:28 +0000

    fix files locked

--- Scripts/CheckSQLFilesLock.ps1
+++ Scripts/CheckSQLFilesLock.ps1
@@ -1,48 +1,59 @@
-function Test-FileLock {
-  param (
-    [parameter(Mandatory=$true)][string]$Path
-  )
-
-  $oFile = New-Object System.IO.FileInfo $Path
-
-  if ((Test-Path -Path $Path) -eq $false) {
-    return $false
-  }
-
-  try {
-    $oStream = $oFile.Open([System.IO.FileMode]::Open, [System.IO.FileAccess]::ReadWrite, [System.IO.FileShare]::None)
-
-    if ($oStream) {
-      $oStream.Close()
-    }
-    return $false
-  } catch {
-    # file is locked by a process.
-    return $true
-  }
+Function Test-IsFileLocked {
+	[cmdletbinding()]
+	Param (
+		[parameter(Mandatory=$True,ValueFromPipeline=$True,ValueFromPipelineByPropertyName=$True)]
+		[Alias('FullName','PSPath')]
+		[string[]]$Path
+	)
+	Process {
+		ForEach ($Item in $Path) {
+			#Ensure this is a full path
+			$Item = Convert-Path $Item
+			#Verify that this is a file and not a directory
+			If ([System.IO.File]::Exists($Item)) {
+				Try {
+					$FileStream = [System.IO.File]::Open($Item,'Open','Write')
+					$FileStream.Close()
+					$FileStream.Dispose()
+					$IsLocked = $False
+				} Catch [System.UnauthorizedAccessException] {
+					$IsLocked = 'AccessDenied'
+				} Catch {
+					$IsLocked = $True
+				}
+				[pscustomobject]@{
+					File = $Item
+					IsLocked = $IsLocked
+				}
+			}
+		}
+	}
 }
+
+
 
 $volumes = Get-Volume | Where-Object {($_.DriveType -in "Fixed") -and ($_.FileSystemLabel -ne "System Reserved")}
 foreach ($v in $volumes){
+	
     #if ($v.DriveLetter -in "C", " ", "" ){
-    if ($v.DriveLetter -in "", " ", "" ){
-        continue;
-    }
-    elseif ($v.DriveLetter -in "C"){
-        $path = "C:\Program Files\Microsoft SQL Server\";
-    }
-    else{
-        $path = "$($v.DriveLetter):\";
-    }
+	if ($v.DriveLetter -in $null, "", " ", "" ){
+		continue;
+	}
+	elseif ($v.DriveLetter -in "C"){
+		$path = "C:\SQL\";
+	}
+	else{
+		$path = "$($v.DriveLetter):\";
+	}
 
     $files = Get-ChildItem -Path "$($path)" -Recurse | Where-Object {$_.extension -in ".mdf",".ldf",".ndf"}
-    foreach ($f in $files){
-        if ($(Test-FileLock $f.FullName) -eq $true){
-            Write-Output "Locked -> $($f.FullName)"
-            continue;
-        }
-        else{
-            Write-Output "Not Locked -> $($f.FullName)"
-        }
-    }
+	foreach ($f in $files){
+		if ($(Test-IsFileLocked $f.FullName).IsLocked -eq $true){
+			Write-Output "Locked -> $($f.FullName)"
+			continue;
+		}
+		else{
+			Write-Output "Not Locked -> $($f.FullName)"
+		}
+	}
 }
